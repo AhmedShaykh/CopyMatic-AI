5570aa4a89c2c14c3b65b7a42c7bac83e9742c43
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-11-14 19:46:29 +0500

    Git Init

--- src/Components/Slide/Slide.jsx
+++ src/Components/Slide/Slide.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
-import Button from '@mui/material/Button';
 
 import './Slide.css';
 
@@ -16,17 +15,34 @@ const Slide = () => {
     return (
         <Box className='slider-box'>
             <Box className='swticher-btn'>
-                <Button>Monthly</Button>
-                <Button>Yearly</Button>
+                <button>Monthly</button>
+                <button>Yearly</button>
+
+                <span className='free'>4 month's free</span>
             </Box>
-            <Slider display="flex" flexDirection="column" alignItems="center"
-                style={{ width: 600 }}
-                min={10}
-                max={100}
-                step={20}
-                price={price}
-                marks
-                onChange={changeprice} />
+            <Box className='slide-text'>
+                <p>
+                    How many words would you like per month?
+                </p>
+            </Box>
+            <Box className='slide'>
+                <p className='left'>15,000</p>
+                <p className='right'>32,000,000</p>
+            </Box>
+            <Box className="slider-area">
+                <Slider display="flex" flexDirection="column" alignItems="center"
+                    style={{ width: 700, color: "#2615C7" }}
+                    min={10}
+                    max={100}
+                    step={20}
+                    price={price}
+                    marks
+                    onChange={changeprice} />
+            </Box>
+            <Box className="pricing-area">
+                <Box className="pricing-top"></Box>
+                <Box className="pricing-bottom"></Box>
+            </Box>
         </Box>
     )
 };
