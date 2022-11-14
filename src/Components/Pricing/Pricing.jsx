d41c5b8c02a18f9f2f09c74206da07dd9a13bee0
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-11-14 17:10:32 +0500

    Git Init

--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -1,8 +1,28 @@
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+
 import './Pricing.css';
 
 const Pricing = () => {
+
+    const [price, SetPrice] = useState(29);
+
+    const changeprice = (event, price) => {
+        SetPrice(price);
+    };
+
     return (
-        <div></div>
+        <Box className='price-box'>
+            <Slider display="flex" flexDirection="column" alignItems="center"
+                style={{ width: 600 }}
+                min={10}
+                max={100}
+                step={20}
+                price={price}
+                marks
+                onChange={changeprice} />
+        </Box>
     )
 };
 
