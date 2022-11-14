4b718d0d9fc45f583756ada32bd7063721e65461
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-11-14 23:44:54 +0500

    Git Init

--- src/Components/Slide/Slide.jsx
+++ src/Components/Slide/Slide.jsx
@@ -65,34 +65,31 @@ const Slide = () => {
                         </Box>
                     </Box>
                 </Box>
-                <Box className="pricing-box pro">
+                <Box className="pricing-box enter">
                     <Box className="pb">
                         <Box className="pricing-top">
-                            <h5>Pro</h5>
+                            <h5>Enterprise</h5>
                             <Box className="price">
-                                <span className="upper">$</span>
-                                <span className="price-number">19</span>
-                                <span className="mo-discount">/mo</span>
-                                <button className='price-btn'>Start Free Trial</button>
+                                <span className="custom">Custom</span>
+                                <button className='sale-btn'>Contact Sales</button>
                             </Box>
                         </Box>
                         <Box className="pricing-body">
                             <ul>
-                                <li>Access to all tools</li>
-                                <li>Unlimited user seats</li>
-                                <li>Unlimited projects</li>
-                                <li>25+ languages</li>
-                                <li>API access</li>
-                                <li>Wordpress plugin</li>
-                                <li>SEO keyword research</li>
-                                <li>Plagiarism checker <span>(5,000 free words)</span></li>
+                                <li>
+                                    <strong className='plus'>Everything in Pro, plus</strong>
+                                </li>
+                                <li>Dedicated account manager</li>
+                                <li>Custom tools</li>
+                                <li>Advanced integrations</li>
+                                <li>Volume-based pricing</li>
+                                <li>Premium technical support</li>
+                                <li>SEO campaign manager</li>
+                                <li>Human editing & proofreading</li>
                             </ul>
                         </Box>
                     </Box>
                 </Box>
-                {/* <Box className="pricing-box enterprise">
-                    <h5>Enterprise</h5>
-                </Box> */}
             </Box>
         </Box>
     )
